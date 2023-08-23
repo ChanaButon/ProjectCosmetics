c106bf21a4e749bf441ce76b4e226fc15993928e
ChanaButon <booton@g.jct.ac.il>
2023-08-23 18:41:26 +0300

    valid

--- client/src/validation/validationUtils.js
+++ client/src/validation/validationUtils.js
@@ -1,7 +1,18 @@
-
-exp
-
-
-
-
+export function isValidPhoneNumber(){
+    if(! /^\d+$/.test(a)){
+        return false
+    }
+    if(a.length < 9 || a.length > 10)
+    {
+        return false
+    }
+    if(a.length === 10 &&  a.startsWith('05')){
+        return true
+    }
+    if(a.length === 9  ){
+        const validPrefixes = ['02', '03', '04', '08', '09'];
+        const startsWithValidPrefix = validPrefixes.some(prefix => a.startsWith(prefix));
+        return startsWithValidPrefix
+    }
+    
 }
