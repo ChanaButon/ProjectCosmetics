3b9c3909f27a393b38aeb3fc5e7d6222bd7f986c
ChanaButon <booton@g.jct.ac.il>
2023-09-03 02:04:38 +0300

    user

--- client/src/professionalComponent/BusinessLogin.js
+++ client/src/professionalComponent/BusinessLogin.js
@@ -1,12 +1,25 @@
 // businessLogin.js
-import React, { useState } from 'react';
+
+import { statSync } from 'fs';
+import React, { useState ,useRef} from 'react';
 import { useLocation } from 'react-router-dom'; // Import the useLocation hook
+import axios from 'axios'
+import { connect } from 'react-redux'
+
+
 
 const BusinessLogin = () => {
   // Use the useLocation hook to get the location object
   const location = useLocation();
   const {product} = location.state || {};
   console.log(product);
+
+
+
+  const dayRef = useRef('null')
+  const  startTimeRef= useRef('null')
+  const  finishTimeRef= useRef('null')
+  const  statusRef= useRef('null')
 
   // State to manage prices for selected treatments
   const [treatmentPrices, setTreatmentPrices] = useState({});
@@ -20,6 +33,42 @@ const BusinessLogin = () => {
     }));
     console.log(treatmentPrices);
   };
+
+
+
+  const submitUser = () =>{
+
+    let userbusiness ={
+      Day:dayRef.current.value,
+      Start:startTimeRef.current.value,
+      End:finishTimeRef.current.value,
+      Status:statusRef.current.value,
+
+        
+
+
+    }
+    console.log(userbusiness)
+
+    axios.post('http://localhost:3321/timeDay/newTimeDay',userbusiness).then((res) => {
+      if (res.data) {
+        console.log(res);
+        //עדכון לסטור
+        // dispatch(setUser(res.data.newProduct))
+        // navigate("/BusinessLogin",{state:{product}});
+      }
+    }).catch((err) => {
+      console.log(err);
+      alert("אירעה שגיאה")
+    })
+    
+
+  }
+
+  
+
+
+  
 
   return (
     <div>
@@ -42,6 +91,17 @@ const BusinessLogin = () => {
           <p>No treatments selected.</p>
         )}
       </ul> }
+      <input   ref={dayRef} placeholder='Day' id="DayInput"  className={`aaa bbb`} type="text" />
+      <input  ref={startTimeRef} placeholder='StaartTime' id="StartTimeInput"  className={`aaa bbb`} type="time" />
+      <input  ref={finishTimeRef} placeholder='FinishTime' id="FinishTimeInput"  className={`aaa bbb`} type="time" />
+      <input  ref={statusRef}  placeholder='Status' id="StatusInput"  className={`aaa bbb`} type="bit" />
+      {/* {{formErrors.familyName && <span className="error-text">שדה חובה</span>} } */}
+      <button onClick={submitUser} color='pink'>Submit</button>
+      
+      
+
+
+
     </div>
   );
 };
